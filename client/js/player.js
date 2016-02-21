4ca195aa709a6e4efb67120c0f15183c3ef4b99b
Felix Beaulieu <felix.beaulieu7@gmail.com>
2016-02-20 20:30:28 -0500

    Clean up of old and temps files

--- client/js/player.js
+++
@@ -1,9 +0,0 @@
-var Player = (function () {
-    function Player() {
-        this.name = '';
-    }
-    Player.prototype.Player = function (name) {
-        this.name = name;
-    };
-    return Player;
-}());
